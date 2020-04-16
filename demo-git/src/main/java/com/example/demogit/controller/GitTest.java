c9d36a766d32cbc3d16798a7a7a53d0873dfac5e
Qxf <909111156@qq.com>
2020-04-16 09:40:36 +0800

    第一次提交Test

---
+++ demo-git/src/main/java/com/example/demogit/controller/GitTest.java
@@ -0,0 +1,16 @@
+package com.example.demogit.controller;
+
+/**
+ * @author Qxf
+ * @version V1.0.0
+ * @Package com.example.demogit.controller
+ * @date 2020/4/16 9:34
+ * @Copyright © 2020-2021
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+
+}
